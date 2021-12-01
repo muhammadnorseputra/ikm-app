de730400652edf002eb4d3fa664642dbb066f0ff
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-12-01 12:07:15 +0700

    adcode adsense

--- application/views/Frontend/skm/layout/head.php
+++ application/views/Frontend/skm/layout/head.php
@@ -43,6 +43,8 @@
 
 	      gtag('config', 'UA-199508931-1');
 	    </script>
+	    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6373503432980668"
+     crossorigin="anonymous"></script>
 	</head>
 	<body id="top" class="position-relative" data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="50">
 	<div class="main">
