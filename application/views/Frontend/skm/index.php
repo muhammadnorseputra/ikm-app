28622ddb70d10673028255eb3d1d5c78ad1a479d
Purtabungsu <muhammadnorseputra@gmail.com>
2022-06-07 14:07:34 +0800

    change hero

--- application/views/Frontend/skm/index.php
+++ application/views/Frontend/skm/index.php
@@ -1,6 +1,6 @@
 <section class="hero">
-	<div class="p-md-4 py-3 px-2 hero-overlay">
-		<div class="container py-md-5 h-full ">
+	<div class="p-md-5 py-4 px-2 hero-overlay">
+		<div class="container py-md-5">
 			<div class="d-flex justify-content-between align-items-md-center align-items-start flex-lg-row flex-column overflow-y-hidden">
 				<div class="order-last order-md-first col-md-6">
 					<!-- <img src="<?= base_url('assets/images/logo.png') ?>" class="d-none d-md-block mt--5" width="80" alt="Logo Kabupaten Balangan - BKPSDM Kab. Balangan"> -->
@@ -29,8 +29,48 @@
 			</div>
 		</div>
 	</div>
+<svg style="pointer-events: none;position: relative;top:-50;" class="wave" width="100%" height="50px" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 75">
+    <defs>
+        <style>
+            .a {
+                fill: none;
+            }
+
+            .b {
+                clip-path: url(#a);
+            }
+
+            .c,
+            .d {
+                fill: #ffffff;
+            }
+
+            .d {
+                opacity: 0.5;
+                isolation: isolate;
+            }
+        </style>
+        <clipPath id="a">
+            <rect class="a" width="1920" height="75"></rect>
+        </clipPath>
+    </defs>
+    <title>wave</title>
+    <g class="b">
+        <path class="c" d="M1963,327H-105V65A2647.49,2647.49,0,0,1,431,19c217.7,3.5,239.6,30.8,470,36,297.3,6.7,367.5-36.2,642-28a2511.41,2511.41,0,0,1,420,48"></path>
+    </g>
+    <g class="b">
+        <path class="d" d="M-127,404H1963V44c-140.1-28-343.3-46.7-566,22-75.5,23.3-118.5,45.9-162,64-48.6,20.2-404.7,128-784,0C355.2,97.7,341.6,78.3,235,50,86.6,10.6-41.8,6.9-127,10"></path>
+    </g>
+    <g class="b">
+        <path class="d" d="M1979,462-155,446V106C251.8,20.2,576.6,15.9,805,30c167.4,10.3,322.3,32.9,680,56,207,13.4,378,20.3,494,24"></path>
+    </g>
+    <g class="b">
+        <path class="d" d="M1998,484H-243V100c445.8,26.8,794.2-4.1,1035-39,141-20.4,231.1-40.1,378-45,349.6-11.6,636.7,73.8,828,150"></path>
+    </g>
+</svg>
 </section>
-<section class="pedoman-ikm mb-5" style="margin-top: -5%;">
+
+<section class="pedoman-ikm mb-5">
 <div class="container">
 	<div class="row align-items-center">
 		<div class="col-12">
