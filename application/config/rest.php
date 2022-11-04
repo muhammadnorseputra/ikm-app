2eac7c827f97f414e4ae24ae30e1167e5a9a2426
muhammadnorseputra <muhammadnorseputra@gmail.com>
2022-11-04 13:59:39 +0700

    rest valid ip

--- application/config/rest.php
+++ application/config/rest.php
@@ -252,7 +252,7 @@ $config['rest_handle_exceptions'] = true;
 | 127.0.0.1 and 0.0.0.0 are allowed by default
 |
 */
-$config['rest_ip_whitelist'] = '127.0.0.1, 192.168.147.60';
+$config['rest_ip_whitelist'] = '127.0.0.1, 192.168.147.60, 120.188.74.108';
 
 /*
 |--------------------------------------------------------------------------
