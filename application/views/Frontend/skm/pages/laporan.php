97c428ef37757a20a8c529daf19ee6c108d6812c
Putrabungsu6 <muhammadnorseputra@gmail.com>
2024-07-22 13:37:18 +0800

    fix error frequensi unsur

--- application/views/Frontend/skm/pages/laporan.php
+++ application/views/Frontend/skm/pages/laporan.php
@@ -347,7 +347,7 @@ $t = !empty($target->target_tahunan) ? $target->target_tahunan : 0;
 							foreach(@$responden_detail as $key => $val):
 								@$jawaban_pecah[] = explode(",",$val->jawaban_responden);
 							endforeach;
-							$marge = array_merge([], ...@$jawaban_pecah);
+							$marge = @array_merge([], ...@$jawaban_pecah);
 							$total = array_count_values($marge);
 							$no=1;
 							foreach($jawaban as $key => $val):
